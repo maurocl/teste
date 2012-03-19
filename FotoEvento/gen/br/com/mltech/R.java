aabc5544e852548cf1b300b7569c3875fbd719f9
maurocl <maurocl@c0c7435a-8005-e111-8f62-00e0815b4126>
2012-03-19 13:47:00 +0000

    Primeira atualização no PC de casa

--- FotoEvento/gen/br/com/mltech/R.java
+++ FotoEvento/gen/br/com/mltech/R.java
@@ -13,11 +13,32 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int btnIncluir=0x7f05000c;
+        public static final int button1=0x7f050003;
+        public static final int editEmail=0x7f050006;
+        public static final int editNome=0x7f050004;
+        public static final int editTelefone=0x7f050008;
+        public static final int radioButton1=0x7f05000a;
+        public static final int radioButton2=0x7f05000b;
+        public static final int textView1=0x7f050000;
+        public static final int textView2=0x7f050001;
+        public static final int textView3=0x7f050002;
+        public static final int txtEmail=0x7f050005;
+        public static final int txtTelefone=0x7f050007;
+        public static final int txtTipoFoto=0x7f050009;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int relatorio=0x7f030001;
+        public static final int usuario=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int email=0x7f040003;
         public static final int hello=0x7f040000;
+        public static final int nome=0x7f040002;
+        public static final int telefone=0x7f040004;
+        public static final int tipofoto=0x7f040005;
     }
 }
