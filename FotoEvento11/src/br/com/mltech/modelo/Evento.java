ce2e45c8000f973a5d5e227e83695cffa18c272b
maurocl <maurocl@c0c7435a-8005-e111-8f62-00e0815b4126>
2012-06-13 00:04:46 +0000

    Alteração da formatação do arquivo e também a introdução de novos métodos getXXX().

--- FotoEvento11/src/br/com/mltech/modelo/Evento.java
+++ FotoEvento11/src/br/com/mltech/modelo/Evento.java
@@ -23,22 +23,31 @@ public class Evento implements Serializable {
   private Contratante contratante;
 
   private String nome; // nome do evento
+
   private String data; // data do evento
+
   private String email; // email principal do evento
 
   private String endereco;
+
   private String cidade;
+
   private String estado;
+
   private String cep;
+
   private String telefone;
 
   private String contaFacebook;
+
   private String contaTwitter;
 
   private boolean enviaFacebook;
+
   private boolean enviaTwitter;
 
   private String bordaPolaroid;
+
   private String bordaCabine;
 
   private Parametros parametros; // indica se o evento terá parâmetros
@@ -51,6 +60,7 @@ public class Evento implements Serializable {
    * Construtor vazio
    */
   public Evento() {
+
     this.parametros = new Parametros();
     this.participantes = new ArrayList<Participacao>();
   }
@@ -63,135 +73,199 @@ public class Evento implements Serializable {
    * 
    */
   public Evento(Contratante c) {
+
     this.contratante = c;
     this.parametros = new Parametros();
   }
 
   public Contratante getContratante() {
+
     return contratante;
   }
 
   public void setContratante(Contratante contratante) {
+
     this.contratante = contratante;
   }
 
   public String getNome() {
+
     return nome;
   }
 
   public void setNome(String nome) {
+
     this.nome = nome;
   }
 
   public String getData() {
+
     return data;
   }
 
   public void setData(String data) {
+
     this.data = data;
   }
 
   public String getEmail() {
+
     return email;
   }
 
   public void setEmail(String email) {
+
     this.email = email;
   }
 
   public String getEndereco() {
+
     return endereco;
   }
 
   public void setEndereco(String endereco) {
+
     this.endereco = endereco;
   }
 
   public String getCidade() {
+
     return cidade;
   }
 
   public void setCidade(String cidade) {
+
     this.cidade = cidade;
   }
 
   public String getEstado() {
+
     return estado;
   }
 
   public void setEstado(String estado) {
+
     this.estado = estado;
   }
 
   public String getCep() {
+
     return cep;
   }
 
   public void setCep(String cep) {
+
     this.cep = cep;
   }
 
   public String getTelefone() {
+
     return telefone;
   }
 
   public void setTelefone(String telefone) {
+
     this.telefone = telefone;
   }
 
   public String getContaFacebook() {
+
     return contaFacebook;
   }
 
   public void setContaFacebook(String contaFacebook) {
+
     this.contaFacebook = contaFacebook;
   }
 
   public String getContaTwitter() {
+
     return contaTwitter;
   }
 
   public void setContaTwitter(String contaTwitter) {
+
     this.contaTwitter = contaTwitter;
   }
 
   public boolean isEnviaFacebook() {
+
     return enviaFacebook;
   }
 
   public void setEnviaFacebook(boolean enviaFacebook) {
+
     this.enviaFacebook = enviaFacebook;
   }
 
+  /**
+   * esse método facilita o trabalho
+   * 
+   * @param enviaFacebook
+   */
+  public void setEnviaFacebook(String enviaFacebook) {
+
+    if (enviaFacebook.equalsIgnoreCase("true")) {
+      this.enviaFacebook = true;
+    }
+    else {
+      this.enviaFacebook = false;
+    }
+
+  }
+
   public boolean isEnviaTwitter() {
+
     return enviaTwitter;
   }
 
   public void setEnviaTwitter(boolean enviaTwitter) {
+
     this.enviaTwitter = enviaTwitter;
   }
 
+  /**
+   * esse método facilita o trabalho
+   * 
+   * @param enviaTwitter
+   */
+  public void setEnviaTwitter(String enviaTwitter) {
+
+    if (enviaTwitter.equalsIgnoreCase("true")) {
+      this.enviaTwitter = true;
+    }
+    else {
+      this.enviaTwitter = false;
+    }
+  }
+
   public String getBordaPolaroid() {
+
     return bordaPolaroid;
   }
 
   public String getBordaCabine() {
+
     return bordaCabine;
   }
 
   public Parametros getParametros() {
+
     return parametros;
   }
 
   public void setParametros(Parametros parametros) {
+
     this.parametros = parametros;
   }
 
   public void setBordaPolaroid(String bordaPolaroid) {
+
     this.bordaPolaroid = bordaPolaroid;
   }
 
   public void setBordaCabine(String bordaCabine) {
+
     this.bordaCabine = bordaCabine;
   }
 
@@ -204,6 +278,7 @@ public class Evento implements Serializable {
    * 
    */
   public ArrayList<Participacao> getParticipantes() {
+
     return participantes;
   }
 
@@ -216,6 +291,7 @@ public class Evento implements Serializable {
    * 
    */
   public void setParticipantes(ArrayList<Participacao> participantes) {
+
     this.participantes = participantes;
   }
 
@@ -224,6 +300,7 @@ public class Evento implements Serializable {
    */
   @Override
   public String toString() {
+
     return "Evento [contratante=" + contratante + ", nome=" + nome + ", data=" + data + ", email=" + email + ", endereco="
         + endereco + ", cidade=" + cidade + ", estado=" + estado + ", cep=" + cep + ", telefone=" + telefone + ", contaFacebook="
         + contaFacebook + ", contaTwitter=" + contaTwitter + ", enviaFacebook=" + enviaFacebook + ", enviaTwitter=" + enviaTwitter
