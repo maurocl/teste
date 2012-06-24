8cf36dc75535309ac13987a58ebab41ff31934d7
maurocl <maurocl@c0c7435a-8005-e111-8f62-00e0815b4126>
2012-06-24 23:42:07 +0000

    Atualização

--- library/src/br/com/mltech/utils/Transaction.java
+++ library/src/br/com/mltech/utils/Transaction.java
@@ -1,10 +1,23 @@
 package br.com.mltech.utils;
 
-
+/**
+ * Transaction
+ * 
+ * @author maurocl
+ *
+ */
 public interface Transaction {
 
+	/**
+	 * executa uma tarefa
+	 * 
+	 * @throws Exception
+	 */
 	public void execute() throws Exception;
 	
+	/**
+	 * atualiza a view
+	 */
 	public void updateView();
 	
 }
