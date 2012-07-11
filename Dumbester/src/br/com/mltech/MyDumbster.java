1579fe32cedc02d5228c703ba9013c03bb12dd0c
maurocl <maurocl@c0c7435a-8005-e111-8f62-00e0815b4126>
2012-07-11 14:33:11 +0000

    Importação inicial

---
+++ Dumbester/src/br/com/mltech/MyDumbster.java
@@ -0,0 +1,12 @@
+package br.com.mltech;
+
+import com.dumbster.smtp.SimpleSmtpServer;
+
+public class MyDumbster {
+
+	public static void main(String[] args) {
+		
+		SimpleSmtpServer.start();
+
+	}
+}
