621ea41216dd907324e40915f00b751c2659738a
maurocl <maurocl@c0c7435a-8005-e111-8f62-00e0815b4126>
2012-09-09 23:01:55 +0000

    Atualização

---
+++ FotoEvento11/src/br/com/mltech/view/SobreActivity.java
@@ -0,0 +1,14 @@
+package br.com.mltech.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SobreActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
