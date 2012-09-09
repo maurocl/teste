a9a11f25e0d997cd347c6c637156b7917a48071e
maurocl <maurocl@c0c7435a-8005-e111-8f62-00e0815b4126>
2012-09-09 23:00:15 +0000

    Novo arquivo

--- FotoEvento11/tools/create_tag.bat
+++ FotoEvento11/tools/create_tag.bat
@@ -1,7 +1,12 @@
+@echo off
+@rem
+@rem create_tag.bat
+@rem
 rem copy -rHEAD https://svn2.sliksvn.com/maurolopes_maurocl/FotoEvento11 https://svn2.sliksvn.com/maurolopes_maurocl/FotoEvento11-tags/FotoEvento11-120907
 
 
 set HOSTNAME=https://svn2.sliksvn.com
 set ACCOUNT=maurolopes_maurocl
+set DIR=
 
-copy -rHEAD %HOSTNAME%/maurolopes_maurocl/%DIR% https://svn2.sliksvn.com/maurolopes_maurocl/FotoEvento11-tags/FotoEvento11-120907
+copy -rHEAD %HOSTNAME%/%ACCOUNT%/%DIR% %HOSTNAME%/%ACCOUNT%/FotoEvento11-tags/FotoEvento11-120907
